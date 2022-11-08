6d46aa1fff060e6d6b8e559d06f3f22b7f0a9cc8
vql <valentinjossic@outlook.fr>
2022-11-08 16:49:19 +0100

    Add multiple visualisations and rolling averages

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 import matplotlib.pylab as plt
 from datetime import *
-
+import matplotlib.dates as mdates
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
 
 #### vql's spaghetti code, unprecedented genius gift of god below ####
 # just kiding have fun reading this bs
@@ -18,8 +21,12 @@ from datetime import *
 # !!! The command is infinite until you stop it: it is a 'while true' command. It gives a result every 30 seconds by default, with a limit rate of 100kbits/s for each instruction
 
 # load the json file
-f = open("results_json/results_04112022.json")
-data = json.load(f)
+
+files = [f for f in listdir('results_json/')
+         if isfile(join('results_json/', f))]
+data = []
+for f in files:
+    data.append(json.load(open('results_json/' + f)))
 
 ### interesting parameters ###
 
@@ -33,25 +40,48 @@ data = json.load(f)
 # time_total
 
 # parameters to consider, up to 7 (restricted by number of colors on plot)
-params = ["speed_download"]
+params = ['time_total']
 
-times = []
-values = [[0.5 for i in range(len(data))] for j in range(len(params))]
+times = [[] for i in range(len(data))]
+
+values = [[[0.5 for k in range(len(data[j]))] for i in range(
+    len(params))] for j in range(len(data))]
 
 # goes through every line of the json file, and gets its time and the requested parameters in lists so they can be used later
 for i in range(len(data)):
-    times.append(datetime.strptime(
-        data[i][1]["timestamp"], "%Y-%m-%dT%H:%M:%S"))
-    for j in range(len(params)):
-        if(not(params[j] == 'time_total' and (data[i][0][params[j]] > 1 or data[i][0][params[j]] < 0.01))):
-            values[j][i] = data[i][0][params[j]]
+    for j in range(len(data[i])):
+        times[i].append(datetime.strptime(
+            data[i][j][1]["timestamp"], "%Y-%m-%dT%H:%M:%S"))
+        for k in range(len(params)):
+            if (not (params[k] == 'time_total' and (data[i][j][0][params[k]] > 2 or data[i][j][0][params[k]] < 0.01))):
+                values[i][k][j] = data[i][j][0][params[k]]
+
 
 # just the plot colors...
 colors = ["r", "b", "g", "c", "m", "y", "k"]
 
 # plots every parameters, function of time
-for i in range(len(params)):
-    plt.plot(times, values[i], color=colors[i], label=params[i])
+
+fig, axs = plt.subplots(int(len(data) / 2), len(data) - int(len(data) / 2))
+
+xformatter = mdates.DateFormatter('%H:%M')
+for i in range(len(data)):
+    for j in range(len(params)):
+        row = 0
+        col = i
+        if(i >= int(len(data) / 2)): 
+            row = 1
+            col = i - int(len(data) / 2)
+        tmp_df = pd.DataFrame({'times': times[i], 'val': values[i][j]})
+        tmp_df = tmp_df.set_index('times')
+        tmp_avg_df = tmp_df.rolling(window=120).mean()
+
+        axs[row, col].plot(tmp_df, color=colors[j], label=params[j])
+        axs[row, col].plot(tmp_avg_df, color=colors[j + 1], label=f"running avg of {params[j]}")
+        axs[row, col].grid()
+        axs[row, col].legend()
+        axs[row, col].set_title(f"values for {times[i][0].date()}", fontsize=8)
+        plt.gcf().axes[i].xaxis.set_major_formatter(xformatter)
 
 plt.legend()
 plt.grid()
